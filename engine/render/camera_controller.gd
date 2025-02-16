550ca2634b4f237e8e8868f8e9692ada2f37c6c3
AnalyticalGoose <95051021+AnalyticalGoose@users.noreply.github.com>
2025-02-16 11:39:55 +0000

    Fix camera controller snapping unpredictability
    Fixes #8 - Camera controller is now more predictable and no longer snaps 180 degrees when trying to rotate on the Y-axis

--- engine/render/camera_controller.gd
+++ engine/render/camera_controller.gd
@@ -16,11 +16,14 @@ func _input(event: InputEvent) -> void:
 			camera.position.z = clamp(camera.position.z * 0.90, 0.1, 20.0)
 		elif Input.is_action_pressed("zoom_out"):
 			camera.position.z = clamp(camera.position.z * 1.1, 0.1, 20.0)
-			
+		
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE) and event is InputEventMouseMotion:
-			camera_pivot.rotate_y(-(event as InputEventMouseMotion).relative.x * ORBIT_SPEED)  # Rotate horizontally
-			camera_pivot.rotate_x(-(event as InputEventMouseMotion).relative.y * ORBIT_SPEED)  # Rotate vertically
-			camera_pivot.rotation.z = 0
+			camera_pivot.rotate_y(-(event as InputEventMouseMotion).relative.x * ORBIT_SPEED)
+			camera_pivot.rotation_degrees.x = clamp(
+					camera_pivot.rotation_degrees.x - (event as InputEventMouseMotion).relative.y, -89, 89
+			)
+			# Reset roll so it doesn't accumulate
+			camera_pivot.rotation_degrees.z = 0
 
 
 func _on_mouse_entered() -> void:
